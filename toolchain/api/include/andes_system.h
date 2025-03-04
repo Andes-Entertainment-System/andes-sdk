7a619a00dbb632be25cd25feda3bf6fdc47b2087
SneezingCactus <sneezingcactus452@gmail.com>
2025-03-04 18:12:19 -0300

    removed SYS_getRandom in favour of stdc rand

--- toolchain/api/include/andes_system.h
+++ toolchain/api/include/andes_system.h
@@ -3,11 +3,6 @@
 #include <stdint.h>
 
 /**
- * @brief Get a pseudo-random 32 bit integer between 0 and the largest possible signed 32 bit value.
- */
-extern int32_t SYS_getRandom();
-
-/**
  * @brief Get the amount of time elapsed since console startup, in microseconds.
  */
 extern uint64_t SYS_getTime();
