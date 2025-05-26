01b25cac720b83c820a0134a40b90ed366c3bb1d
SneezingCactus <sneezingcactus452@gmail.com>
2025-05-26 16:58:56 -0300

    fixed TIL_setPlaneHScrollTable not working past row 0

--- toolchain/api/src/andes_graphics_tiles.c
+++ toolchain/api/src/andes_graphics_tiles.c
@@ -8,6 +8,6 @@ void TIL_loadTileSetAt(struct TileSetResource* res, uint32_t offset) {
 }
 
 void TIL_setPlaneHScrollTable(enum TilePlane plane, uint32_t row, uint32_t* offsets, uint32_t n) {
-  STO_copyPtrToRegister(plane == TILEPLANE_FG ? REG_FG_HSCROLL_TABLE : REG_BG_HSCROLL_TABLE, row, offsets,
-                        n * sizeof(uint32_t));
+  STO_copyPtrToRegister(plane == TILEPLANE_FG ? REG_FG_HSCROLL_TABLE : REG_BG_HSCROLL_TABLE, row * sizeof(uint32_t),
+                        offsets, n * sizeof(uint32_t));
 }
