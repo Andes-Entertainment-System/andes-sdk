e8e7e53bec5f3e4863e6465d4c071d15fa0d7942
SneezingCactus <sneezingcactus452@gmail.com>
2025-05-26 16:54:49 -0300

    xtensa target changes

--- toolchain/cmake/target-xtensa.cmake
+++ toolchain/cmake/target-xtensa.cmake
@@ -1,2 +1,7 @@
-set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -O2)
-set(CMAKE_EXE_LINKER_FLAGS "-Wl,-r -nostartfiles -nodefaultlibs -nostdlib -g")
+# include($ENV{IDF_PATH}/tools/cmake/idf.cmake)
+
+add_compile_definitions("_GNU_SOURCE")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -specs=sim.elf.specs -O2 -fno-jump-tables")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -specs=sim.elf.specs -fno-jump-tables")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-r -nostartfiles -nodefaultlibs -nostdlib -g")
+# target_link_libraries(app.bin idf::newlib)
