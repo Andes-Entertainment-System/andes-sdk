06b4e512f79c50db36cbca9fb85a40207f8851aa
SneezingCactus <sneezingcactus452@gmail.com>
2025-10-04 01:45:56 -0300

    added I_SYS_loadApp

--- toolchain/api/include/andes_system.h
+++ toolchain/api/include/andes_system.h
@@ -6,3 +6,8 @@
  * @brief Get the amount of time elapsed since console startup, in microseconds.
  */
 extern uint64_t SYS_getTime();
+
+/**
+ * @brief (ONLY AVAILABLE TO SYSTEM MENU) Load the specified app.
+ */
+extern void I_SYS_loadApp(char* path);
